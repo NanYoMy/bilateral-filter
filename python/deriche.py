309837f64220fbac35a6ad9fdfee27a57ff611ac
cherhavla <cherhavla@gmail.com>
2013-05-31 02:29:01 +0400

    deriche.py vectorized

--- python/deriche.py
+++ python/deriche.py
@@ -4,7 +4,7 @@ from math import exp
 from scipy import ndimage
 from scipy import misc
 
-from numpy import ndarray
+from numpy import ndarray, repeat
 
 # misc.imsave('./lena.png', lena) # uses the Image module (PIL)
 # blurred_lena = ndimage.gaussian_filter(lena, sigma=3)
@@ -22,31 +22,29 @@ def treat(image, k, a, b, c):
     out2 = ndarray(shape = (n,m), dtype=float)
 
     # from left to right
-    for i in range(n):
-        for j in range(m):
-            if j > 1:
-                out1[i,j] = a[0] * image[i,j] + a[1] * image[i,j-1] + b[0] * out1[i,j-1] + b[1] * out1[i,j-2]
-            if j == 0:
-                out1[i,j] = a[0] * image[i,j]
-            if j == 1:
-                out1[i,j] = a[0] * image[i,j] + a[1] * image[i,j-1] + b[0] * out1[i,j-1]
+    for j in range(m):
+        if j > 1:
+            out1[:,j] = a[0] * image[:,j] + a[1] * image[:,j-1] + b[0] * out1[:,j-1] + b[1] * out1[:,j-2]
+        if j == 0:
+            out1[:,j] = a[0] * image[:,j]
+        if j == 1:
+            out1[:,j] = a[0] * image[:,j] + a[1] * image[:,j-1] + b[0] * out1[:,j-1]
 
     # from right to left
-    for i in range(n):
-        for j in reversed(range(m)):
-            if j < m-2:
-                out2[i,j] = a[2] * image[i,j+1] + a[3] * image[i,j+2] + b[0] * out2[i,j+1] + b[1] * out2[i,j+2]
-            if j == m-1:
-                pass
-            if j == m-2:
-                out2[i,j] = a[2] * image[i,j+1] + b[0] * out2[i,j+1]
+    for j in reversed(range(m)):
+        if j < m-2:
+            out2[:,j] = a[2] * image[:,j+1] + a[3] * image[:,j+2] + b[0] * out2[:,j+1] + b[1] * out2[:,j+2]
+        if j == m-1:
+            pass
+        if j == m-2:
+            out2[:,j] = a[2] * image[:,j+1] + b[0] * out2[:,j+1]
     return (out1 + out2) * c
 
 
 def deriche_filter(image, alpha):
 
     (n, m) = image.shape
-    print('Deriche-filtering image ' +  str(n) + 'x' + str(m) + ' with alpha = ' + str(alpha))
+    # print('Deriche-filtering image ' +  str(n) + 'x' + str(m) + ' with alpha = ' + str(alpha))
 
     # Initialize parameters for smoothing mode
     k = ((1.0 - exp(-alpha)) ** 2.0) / (1.0 + 2.0 * alpha * exp(-alpha) - exp(-2.0 * alpha))
@@ -72,7 +70,7 @@ def deriche_filter(image, alpha):
 
 def main():
     lena = misc.lena()
-    for alpha in [0.1, 0.25, 0.5, 1, 2]:
+    for alpha in [100]:
         dericheLena = deriche_filter(lena, alpha)
         misc.imsave('./deriche_lena_' + str(alpha) + '.png', dericheLena)
 
