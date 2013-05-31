6f5ef46714c7276eee55db961fb53cd4c87f246f
Dan Stefanov <staff.dan@gmail.com>
2013-05-31 10:44:24 +0400

    reference image creating was exclude to dedicated script due to long computing

--- matlab/run.m
+++ matlab/run.m
@@ -6,19 +6,13 @@
 
 % input data
 in_image = imread ('in.bmp');
-
-% setup sigma
-sigma.d = 40;
-sigma.r = 40;
-radius = 100;
+ref_image = imread ('ref.bmp');
 
 % filltering
 filt_image = bf_opt (in_image, sigma);
-ref_image = bf_ref (in_image, sigma, radius);
 
 % writing result
 imwrite (filt_image, 'filt.bmp');
-imwrite (ref_image, 'ref.bmp');
 
 % optimization algorith error
 filt_error = norm(double(filt_image-ref_image)) / norm(double(ref_image));
